19e672eba80b0e08b54144541451618a085f3435
nekolau <1474684244@qq.com>
2018-10-10 14:04:19 +0800

    3

---
+++ 1010.py
@@ -0,0 +1,9 @@
+my_list=["你好",20101010,2019,2019,2019,"Python",2019]
+print(len(my_list))
+print(my_list[0])
+my_list.append("hello")
+print(my_list)
+my_list.remove("Python")
+print(my_list)
+
+print(my_list.count(2019))
