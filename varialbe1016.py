79d99ca8814ac8d3a88dd28d22203f7a525e6e54
nekolau <1474684244@qq.com>
2018-10-17 13:12:49 +0800

    formatting symbol

---
+++ varialbe1016.py
@@ -0,0 +1,16 @@
+# %s: 输出字符串
+# %d:输出int类别数字
+# %f:输出浮点数
+# %x:输出16进制数据
+
+name="张三李四"
+print("我叫%s" % name)
+
+score=100
+print("考试分数：%d" % score)
+
+pi=3.1415926
+print("圆周率：%f" % pi) #%f 默认小数保留6位
+
+num=18
+print("%x" % num)
