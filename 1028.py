91c4b4ae7ee38f118ad43798d8498ccf5fc0c90b
nekoly <nekoly140803@gmail.com>
2018-10-28 23:39:05 +0800

    1028-list

---
+++ 1028.py
@@ -0,0 +1,14 @@
+neko_list = ["渣男","前任","准现任","后任"]
+print(neko_list)
+
+# 列表增加数据
+neko_list.append("IT男")
+print(neko_list)
+
+# 插入指定数据
+neko_list.insert(2,"小鲜肉")
+print(neko_list)
+
+my_boyfriend = ["trader","coder","IT guy"]
+neko_list.append(my_boyfriend)
+print(neko_list)
