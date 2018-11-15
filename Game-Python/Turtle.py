7d2f3aa23b1d53d532cd20397d0adc38e6ea1901
nekolau <1474684244@qq.com>
2018-11-15 14:04:44 +0800

    new file

---
+++ Game-Python/Turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+neko_turtle = turtle.Turtle()
+
+neko_turtle.shape("turtle")
+
+neko_turtle.forward(100)
+neko_turtle.right(90)
+neko_turtle.forward(100)
+neko_turtle.right(90)
+neko_turtle.forward(100)
+neko_turtle.right(90)
+neko_turtle.forward(100)
+
+turtle.mainloop()
+
+
