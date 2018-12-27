e2f85e7f0e8fc89d4b1258d87d6bef73a49b6d27
nekoly <nekoly140803@gmail.com>
2018-12-27 23:31:59 +0800

    1227

---
+++ daily practice/2018-12/1227.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8- -*-
+
+def converter(wight):
+    ponds=wight/0.45
+    print(ponds)
+converter(100)
+
+
+def neko(wight):
+    a=wight/0.25+1
+    print(a)
+neko(66.39)
+neko(34)
+neko(555)
+neko(888)
+
+def neko01(wight=1.25):
+    a=wight/4+1
+    print(a)
+neko01(wight=8)
+
+
+
+
+
+
