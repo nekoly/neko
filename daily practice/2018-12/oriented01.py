ce5eb4722412bc55e70666885c9d9a76002b94c1
nekoly <nekoly140803@gmail.com>
2018-12-27 23:27:31 +0800

    1227

---
+++ daily practice/2018-12/oriented01.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8- -*-
+
+# def func_neko():
+#     print("hello,hk")
+#
+# func_neko()
+
+# a=10
+# b=11
+# print(a+b)
+#
+# c=20
+# d=21
+# print(c+d)
+
+# def sum(a,b):
+#     print(a+b)
+# sum(3,4)
+# sum(45,3)
+
+# def fun():
+#     return"hello,neko"
+# a=fun()
+# print(a)
+#
+# def sum(a,b):
+#     return(a+b)
+# c=sum(3,5)
+# print(c)
+#
+# def w(a,b,c):
+#     return(a*b/c)
+# neko=w(3,4,5)
+# print(neko)
+#
+# def su():
+#     return"hello,neko"
+# a=su()
+# print(a)
+
+def neko(rate):
+    a=rate*6.8
+    print(a)
+neko(554.28)
