2ff4087f4c386322bf18e6030ffdc8ea3fc3ffe5
nekolau <1474684244@qq.com>
2019-01-02 09:56:54 +0800

    190102

--- daily practice/2018-12/1228.py
+++ daily practice/2018-12/1228.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8- -*-
 
+def neko():
+    return"hello,neko"
+a=neko()
+print(a)
 
+
+def neko(a,b,c):
+    return(a+b-c)
+a=neko(3,4,5)
+print(a)
+
+def neko(wight):
+    a=wight/3
+    print(a)
+neko(388)
+
+def neko(wight=88):
+    a=wight/3
+    print(a)
+neko(100)
