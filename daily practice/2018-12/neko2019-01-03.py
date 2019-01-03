fe6744bfe8078561e3e1229971e2c1410414f5fa
nekolau <1474684244@qq.com>
2019-01-03 13:44:59 +0800

    neko2019

--- daily practice/2018-12/neko2019-01-03.py
+++ daily practice/2018-12/neko2019-01-03.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-from random import randint
-from time import sleep
 if __name__ == '__main__':
      unicornHeader = {
         'Referer': 'https://www.ikjzd.com/',
@@ -10,32 +8,25 @@ if __name__ == '__main__':
      }
      # 排名
      i = 1;
-     for i in range(1, 384):
+     for i in range(1, 19):
          print(i);
-
-         #0102
-         sleep(randint(1, 8))
+         # xxx
          url = "https://www.ikjzd.com/a/14242.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
-         sleep(randint(1, 8))
          url = "https://www.ikjzd.com/a/14237.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
-         sleep(randint(1, 8))
          url = "https://www.ikjzd.com/a/14235.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
-         sleep(randint(1, 8))
          url = "https://www.ikjzd.com/a/14224.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
          #0103
-         sleep(randint(1, 8))
-         url = "https://www.ikjzd.com/a/14331.html"
+         url = "https://www.ikjzd.com/a/14224.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
-         sleep(randint(1, 8))
          url = "https://www.ikjzd.com/a/14327.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
 
---
+++ daily practice/2019-01/190103-oriented.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class neko():
+    def face(self):
+        print("face is round")
+    def hair(self):
+        print("hair is black")
+
+class neko01():
+    def wight(self):
+        print("45KG")
+    def height(self):
+        print("158cm")
+
+neko01().wight()
+ly=
